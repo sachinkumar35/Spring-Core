77407b53669111fd94a752e9e097f59898f0827c
Dell <Dell@intel>
2022-12-08 08:37:39 +0530

    First Day

---
+++ springCore/src/main/java/com/springCore/App.java
@@ -0,0 +1,29 @@
+package com.springCore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+//        System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        Student student1 = (Student) context.getBean("student1");
+        Student student2 = (Student) context.getBean("student2");
+        
+        System.out.println(student1);
+        System.out.println(student2);
+        
+//    	Student obj =  new Student();
+//    	obj.setStudentId(100);
+//    	System.out.println(obj.getStudentId());
+//    	obj.setStudentId(101);
+//    	System.out.println(obj.getStudentId());
+        
+    }
+}
